8ad571a9de49d367a5cc61ca6815ad79450476f0
Tommy <60700831+Blurpyx@users.noreply.github.com>
2021-05-06 11:30:49 +0200

    version update

--- public/scripts/db_add.js
+++ public/scripts/db_add.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function () {
     var Bewerking = document.getElementById('bewerking');
+    var col = db.collection('optellingen');
+
     {
         var optaftDiv = document.getElementById('opt-aft');
         var delDiv = document.getElementById('del');
@@ -27,17 +29,17 @@ window.addEventListener('load', function () {
 
     Term_1.addEventListener('input', function() {
         if (Term_2.value != '') {
-            if (Bewerking.value == 'optelling')
+            if (Bewerking.value == 'optellingen')
                 Som.value = parseInt(Term_1.value) + parseInt(Term_2.value);
-            if (Bewerking.value == 'verschil')
+            if (Bewerking.value == 'verschillen')
                 Verschil.value = parseInt(Term_1.value) - parseInt(Term_2.value);
         }
     });
     Term_2.addEventListener('input', function() {
         if (Term_1.value != '') {
-            if (Bewerking.value == 'optelling')
+            if (Bewerking.value == 'optellingen')
                 Som.value = parseInt(Term_1.value) + parseInt(Term_2.value);
-            if (Bewerking.value == 'verschil')
+            if (Bewerking.value == 'verschillen')
                 Verschil.value = parseInt(Term_1.value) - parseInt(Term_2.value);
         }
     });
@@ -70,10 +72,11 @@ window.addEventListener('load', function () {
     
     Bewerking.addEventListener('change', function (e) {
         console.log(`Bewerking veranderd ${e.target.value}`);
+
         var min = document.getElementById('minteken');
         var plus = document.getElementById('plusteken');
 
-        if (e.target.value == 'optelling') {
+        if (e.target.value == 'optellingen') {
             optaftDiv.style.display = 'block';
             delDiv.style.display = 'none';
             vermDiv.style.display = 'none';
@@ -87,7 +90,7 @@ window.addEventListener('load', function () {
             document.getElementById('term2').style.marginLeft = '0';
 
 
-        } else if (e.target.value == 'verschil') {
+        } else if (e.target.value == 'verschillen') {
             optaftDiv.style.display = 'block';
             delDiv.style.display = 'none';
             vermDiv.style.display = 'none';
@@ -101,18 +104,56 @@ window.addEventListener('load', function () {
             document.getElementById('term2').style.marginLeft = '2.27px';
 
 
-        } else if (e.target.value == 'deling') {
+        } else if (e.target.value == 'delingen') {
             delDiv.style.display = 'block';
             optaftDiv.style.display = 'none';
             vermDiv.style.display = 'none';
 
 
-        } else if (e.target.value == 'vermenigvuldiging') {
+        } else if (e.target.value == 'vermenigvuldigingen') {
             vermDiv.style.display = 'block';
             optaftDiv.style.display = 'none';
             delDiv.style.display = 'none';
 
 
+        }
+    });
+
+    document.getElementById('bewerking-toevoegen-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        var form = e.target;
+        var bew = form.bewerking.value;
+        // console.log(bew);
+        col = db.collection(bew);
+        if(bew == 'optellingen') {
+            col.doc(form.term1.value + ' + ' + form.term2.value + ' = ' + form.som.value).set({
+                beschrijving: 'Term 1 + Term 2 = Som',
+                term1: form.term1.value,
+                term2: form.term2.value,
+                som: form.som.value
+            });
+        } else if(bew == 'verschillen') {
+            col.doc(form.term1.value + ' - ' + form.term2.value + ' = ' + form.verschil.value).set({
+                beschrijving: 'Term 1 - Term 2 = Verschil',
+                term1: form.term1.value,
+                term2: form.term2.value,
+                verschil: form.verschil.value
+            });
+        } else if(bew == 'delingen') {
+            col.doc(form.deeltal.value + ' : ' + form.deler.value + ' = ' + form.quotient.value + ' + ' + form.rest.value).set({
+                beschrijving: 'Deeltal : Deler = Quotiënt + Rest',
+                deeltal: form.deeltal.value,
+                deler: form.deler.value,
+                quot: form.quotient.value,
+                rest: form.rest.value
+            });
+        } else if(bew == 'vermenigvuldigingen') {
+            col.doc(form.factor1.value + ' x ' + form.factor2.value + ' = ' + form.product.value).set({
+                beschrijving: 'Factor 1 x Factor 2 = Product',
+                factor1: form.factor1.value,
+                factor2: form.factor2.value,
+                prod: form.product.value
+            });
         }
     });
 });
